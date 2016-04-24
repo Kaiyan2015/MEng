bf11307bd21bcd5240b4abf89d687a505cefa6c4
Kaiyan <raulsjtu@gmail.com>
2016-04-24 16:07:04 -0400

    Update simple_output.py

--- simple_output.py
+++ simple_output.py
@@ -19,24 +19,57 @@ if __name__ == "__main__":
 		if operation == 'q':
 			break
 		elif operation == '0':
-			for i range(8):
+			for i in range(8):
 				GPIO.OUTPUT(GPIO_OUT[i], 0)
 		elif operaion == '1':
-			GPIO.OUTPUT(GPIO_OUT[0], 1)
+			for i in range(8):
+				if i == 1:
+					GPIO.OUTPUT(GPIO_OUT[i-1], 1)
+				else:
+					GPIO.OUTPUT(GPIO_OUT[i], 0)
+			
 		elif operaion == '2':
-			GPIO.OUTPUT(GPIO_OUT[1], 1)
+			for i in range(8):
+				if i == 2:
+					GPIO.OUTPUT(GPIO_OUT[i-1], 1)
+				else:
+					GPIO.OUTPUT(GPIO_OUT[i], 0)
 		elif operaion == '3':
-			GPIO.OUTPUT(GPIO_OUT[2], 1)
+			for i in range(8):
+				if i == 3:
+					GPIO.OUTPUT(GPIO_OUT[i-1], 1)
+				else:
+					GPIO.OUTPUT(GPIO_OUT[i], 0)
 		elif operaion == '4':
-			GPIO.OUTPUT(GPIO_OUT[3], 1)
+			for i in range(8):
+				if i == 4:
+					GPIO.OUTPUT(GPIO_OUT[i-1], 1)
+				else:
+					GPIO.OUTPUT(GPIO_OUT[i], 0)
 		elif operaion == '5':
-			GPIO.OUTPUT(GPIO_OUT[4], 1)
+			for i in range(8):
+				if i == 5:
+					GPIO.OUTPUT(GPIO_OUT[i-1], 1)
+				else:
+					GPIO.OUTPUT(GPIO_OUT[i], 0)
 		elif operaion == '6':
-			GPIO.OUTPUT(GPIO_OUT[5], 1)
+			for i in range(8):
+				if i == 6:
+					GPIO.OUTPUT(GPIO_OUT[i-1], 1)
+				else:
+					GPIO.OUTPUT(GPIO_OUT[i], 0)
 		elif operaion == '7':
-			GPIO.OUTPUT(GPIO_OUT[6], 1)
+			for i in range(7):
+				if i == 1:
+					GPIO.OUTPUT(GPIO_OUT[i-1], 1)
+				else:
+					GPIO.OUTPUT(GPIO_OUT[i], 0)
 		elif operaion == '8':
-			GPIO.OUTPUT(GPIO_OUT[7], 1)
+			for i in range(8):
+				if i == 8:
+					GPIO.OUTPUT(GPIO_OUT[i-1], 1)
+				else:
+					GPIO.OUTPUT(GPIO_OUT[i], 0)
 		elif operaion == '9':
 			for i in range(8):
 				GPIO.OUTPUT(GPIO_OUT[i], 1)
